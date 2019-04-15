d142f41265dc49cb3089fa036b7f25b2a3f63f5e
tatecarson <tatecarson@gmail.com>
2019-04-15 11:34:19 -0500

    check for safari on iphone

--- checkMobile.js
+++ checkMobile.js
@@ -4,13 +4,31 @@ if (md.mobile()) {
   document.getElementById('recorded-walk').style.display = 'none';
 }
 
-console.log(md.mobile()); // 'Sony'
-console.log(md.phone()); // 'Sony'
-console.log(md.tablet()); // null
-console.log(md.userAgent()); // 'Safari'
-console.log(md.os()); // 'AndroidOS'
-console.log(md.is('iPhone')); // false
-console.log(md.is('bot')); // false
-console.log(md.version('Webkit')); // 534.3
-console.log(md.versionStr('Build')); // '4.1.A.0.562'
-console.log(md.match('playstation|xbox')); // false
+if (md.mobile() === 'iPhone' && md.userAgent() !== 'Safari') {
+  document.body.innerHTML = `
+      <section class="hero is-primary is-medium">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Walking
+          </h1>
+          <h2 class="subtitle">
+            It looks like you are using ${md.userAgent()}.
+            Please use Safari for this app to work.
+          </h2>
+        </div>
+      </div>
+    </section>
+  `;
+}
+
+// console.log(md.mobile()); // 'Sony'
+// console.log(md.phone()); // 'Sony'
+// console.log(md.tablet()); // null
+// console.log(md.userAgent()); // 'Safari'
+// console.log(md.os()); // 'AndroidOS'
+// console.log(md.is('iPhone')); // false
+// console.log(md.is('bot')); // false
+// console.log(md.version('Webkit')); // 534.3
+// console.log(md.versionStr('Build')); // '4.1.A.0.562'
+// console.log(md.match('playstation|xbox')); // false
